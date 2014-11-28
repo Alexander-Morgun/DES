cf8d363bcd740b768799c462f7832b9b5ac7e648
Alexander Morgun <alexander_morgun@e1.ru>
2014-11-28 14:21:15 +0600

    E permutation

--- print_permutations.py
+++ print_permutations.py
@@ -108,3 +108,24 @@ print_permutation(PC_2,
                   to_memory=False,
                   src_part_size=28,
                   dst_part_size=24);
+E = (
+32,  1,  2,  3,  4,  5,
+ 4,  5,  6,  7,  8,  9,
+ 8,  9, 10, 11, 12, 13,
+12, 13, 14, 15, 16, 17,
+16, 17, 18, 19, 20, 21,
+20, 21, 22, 23, 24, 25,
+24, 25, 26, 27, 28, 29,
+28, 29, 30, 31, 32,  1,
+)
+E = [i - 1 for i in E]
+print_permutation(E,
+                  "edi",
+                  "; error",
+                  "ecx",
+                  "edx",
+                  open("./E BIT-SELECTION.txt", "w"),
+                  from_memory=False,
+                  to_memory=False,
+                  src_part_size=32,
+                  dst_part_size=24);
